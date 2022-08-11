9efd1ad28e132296ee819ba674bda363f5b52bfa
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-11 13:03:23 +0530

    Adding Data

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,34 +1,58 @@
 package com.bridgelabz;
 
-class Node{
-    int key;
-    Node next;
+class Node
+{
+    public int key;
+    public Node next;
     //constructor
-    public Node(int key) {
-        this.key = key;
-        this.next=null;
+    public Node(int data)
+    {
+        this.key = data;
+        this.next = null;
     }
 }
-public class LinkedList
-{
-    public static void main(String[] args)
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Linked List Program.");
+        //taking object of operation class and passing the data
+        Operations operations = new Operations();
+        operations.add(70);
+        operations.add(30);
+        operations.add(56);
+        //Calling the print method inside main method
+        operations.print();
+    }
+}
+//class for operations
+class Operations {
+    //declaration
+    Node head, tail;
+    //Created Method For adding Element in Linked List
+    public void add(int data)
     {
-        System.out.println("Welcome to Linked List Program.");
-        //assigning values to the nodes
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        //declaring
-        Node head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        Node tail = thirdNode;
-        //Initially temp is head
-        Node temp = head;
-        while (temp != null)
+        Node node = new Node(data);
+        if (head == null)
         {
-            System.out.println(temp.key);
-            temp = temp.next;
+            head = node;
+            tail = node;
+        } else
+        {
+            Node temp = head;
+            this.head = node;
+            node.next = temp;
+        }
+    }
+    //print method
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List Is Empty");
+        } else {
+            Node temp = head;
+            while (temp != null)
+            {
+                System.out.println(temp.key);
+                temp = temp.next;
+            }
         }
     }
 }
